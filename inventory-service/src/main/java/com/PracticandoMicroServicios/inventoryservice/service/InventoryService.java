e8a73e8ab00c3352cdbc446b7259b3d796f2d9ae
Maxi Vulcano <maxivulcano12@gmail.com>
2023-07-20 16:53:32 -0300

    Siguiendo el proyecto

--- inventory-service/src/main/java/com/PracticandoMicroServicios/inventoryservice/service/InventoryService.java
+++ inventory-service/src/main/java/com/PracticandoMicroServicios/inventoryservice/service/InventoryService.java
@@ -1,9 +1,12 @@
 package com.PracticandoMicroServicios.inventoryservice.service;
 
+import com.PracticandoMicroServicios.inventoryservice.dto.InventoryResponse;
 import com.PracticandoMicroServicios.inventoryservice.repository.InventoryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -12,7 +15,13 @@ public class InventoryService {
     private final InventoryRepository inventoryRepository;
 
     @Transactional(readOnly = true)
-    public boolean isInStock(String skuCode) {
-        return inventoryRepository.findBySkuCode(skuCode).isPresent();
+    public List<InventoryResponse> isInStock(List<String> skuCode) {
+
+        return inventoryRepository.findBySkuCodeIn(skuCode).stream()
+                .map(inventory ->
+                    InventoryResponse.builder().skuCode(inventory.getSkuCode())
+                            .isInStock(inventory.getStock() > 0)
+                            .build()
+                ).toList();
     }
 }
--- order-service/src/main/java/com/practicandoMicroServicios/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/practicandoMicroServicios/orderservice/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.practicandoMicroServicios.orderservice.service;
 
+import com.practicandoMicroServicios.orderservice.dto.InventoryResponse;
 import com.practicandoMicroServicios.orderservice.dto.OrderLineItemsDto;
 import com.practicandoMicroServicios.orderservice.dto.OrderRequest;
 import com.practicandoMicroServicios.orderservice.model.Order;
@@ -8,7 +9,9 @@ import com.practicandoMicroServicios.orderservice.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
@@ -18,6 +21,7 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final WebClient webClient;
 
     public void placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
@@ -30,6 +34,25 @@ public class OrderService {
 
         order.setOrderLineItemsList(orderLineItems);
 
+        List<String> skuCodes = order.getOrderLineItemsList().stream().map(OrderLineItems::getSkuCode)
+                .toList();
+        //Call Inventory Service, and place order if product is in
+        //stock
+        InventoryResponse[] inventoryResponseArray = webClient.get()
+                .uri("http://localhost:8083/api/inventory",uriBuilder -> uriBuilder.queryParam("skuCode",skuCodes).build())
+                .retrieve()
+                .bodyToMono(InventoryResponse[].class)
+                .block();
+
+        boolean allProductsInStock = Arrays.stream(inventoryResponseArray)
+                .allMatch(InventoryResponse::isInStock);
+
+
+        if (allProductsInStock){
+            orderRepository.save(order);
+        }else {
+            throw new IllegalArgumentException("Product is not in stock");
+        }
         orderRepository.save(order);
     }
 
